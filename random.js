778ebc748fb7494461d5b1245c30e90044eedb59
Ranjeet Verma <iranjeetverma@gmail.com>
2016-08-30 20:41:07 +0530

    change puzzle

--- random.js
+++ random.js
@@ -1,64 +1,122 @@
 var leftNumber = 0;
 var rightNumber = 0;
 var answer = 0;
+var first_option=0;
+var second_option=0;
 var correctAnwers = 0;
 var total = 0;
 var countDown;
 var timer = 5000;
 var difficulty = 15;
+var multiplication_difficulty = 2;
 var progress;
+var start;
+var OPERATION = {
+		ADD: "addition",
+		SUBS: "substraction",
+		MULTI: "multiplication"
+	}
+var operation = OPERATION.ADD;
 
  $(document).ready(function () {
- 	$("#input-block-result").focus();
-    $("#input-block-result").keyup(function (e) {
-        if (e.keyCode == 13) {
-        	if($("#input-block-result").val() == answer){
-				correctAnwers++;				
-        	}        	
-			clearInterval(countDown);
-			startGame();
-        	$("#input-block-result").val("")
-        }
+    $(".answer-option").click(function (e) {
+		clearInterval(countDown);
+		startGame();
     });
 });
-$(document).click(function(){
-	$("#input-block-result").focus();
+
+$(".button").click(function(e){
+	operation = $(e.target).val();
+	clearInterval(countDown);
+	startGame();
 })
-var changeNumber = function(){
-	countDown = setInterval(changeNumberValue,timer)
+
+var startCountDown = function(){
+	countDown = setInterval(changeQuestion,timer)
 }
 
 function getRandomNumber(){
-	return Math.floor(Math.random()*difficulty)
+	var num = Math.floor(Math.random()*difficulty)
+	while(num == 0){
+		num = Math.floor(Math.random()*difficulty)
+	}
+	return num;
 }
+
 function showProgress(){
 	var sec = timer/10;
-	var progressWidth = 500/sec;
-	$(".progress").css('width',progressWidth+'px');
+	var progressWidth = 100/sec;
+	$(".progress").css('width',progressWidth+'%');
 	progress = setInterval(function(){
-		progressWidth = progressWidth + 500/sec;
-		$(".progress").css('width',progressWidth+'px');
+		progressWidth = progressWidth + 100/sec;
+		$(".progress").css('width',progressWidth+'%');
 
 	},10)
 }
 
-var changeNumberValue = function(){
+var changeQuestion = function(){
 	leftNumber = getRandomNumber()
 	rightNumber = getRandomNumber()
-
-	document.getElementById('left-block').innerHTML = leftNumber;
-	document.getElementById('right-block').innerHTML = rightNumber;
-	answer = leftNumber+rightNumber;	
+	var displayOp = "+"
+	switch(operation){
+		case "substraction" : 
+			displayOp = "-";
+			leftNumber = Math.max(leftNumber, rightNumber)
+			rightNumber = Math.min( rightNumber , leftNumber)	
+			break;
+		case "multiplication" :
+		displayOp = "x";
+		leftNumber = Math.floor(getRandomNumber()/multiplication_difficulty);
+		rightNumber = Math.floor(getRandomNumber()/multiplication_difficulty);
+		break;
+	}
+	$('#left-block').html(leftNumber);
+	$('#op-block').html(displayOp);
+	$('#right-block').html(rightNumber);
 	updateScore();
 	clearInterval(progress)
 	showProgress();
+	displayAnswers();
 	total ++;
 }
-function startGame(){
-	changeNumberValue();
-	changeNumber(); 
+
+var displayAnswers = function(){
+	var options = getAnswerOptions();	
+	$('#first-option').html(options.first_option);
+	$('#second-option').html(options.second_option);
+	$('#third-option').html(options.third_option);
 }
+
+
 function updateScore(){
 	$("#points").html(correctAnwers+" / "+total);
+}
+
+function getAnswerOptions (){
+	var answer = 0;
+	switch(operation){
+		case "addition" : 
+			answer = leftNumber+rightNumber;
+			break;
+		case "substraction" : 
+			answer = leftNumber-rightNumber;
+			break;
+		case "multiplication" :
+		answer = leftNumber*rightNumber;
+		break;
+	}
+	var options =[answer, (answer + getRandomNumber()), Math.abs(answer - getRandomNumber())];
+	var pos = getRandomNumber()%3;
+	options.splice(pos,0,answer);
+	options.shift();
+	return{
+		"first_option": options[0],
+		"second_option": options[1],
+		"third_option": options[2]
+	}
+}
+function startGame(){
+	changeQuestion();	
+	startCountDown();
 }
 startGame();
